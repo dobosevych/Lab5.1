7bd6d6695bf3c8e6e10abd7bee56508034e3ae3d
dobosevych <dobosevych@gmail.com>
2022-10-13 11:20:06 +0300

    Fixed CI

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
-        boolean f = true || false;
-        for(int i = 0; i < 10; i++) {
-            System.out.println("Hello!");
-        }
-    }
-}
--- src/main/java/flower_store/FlowerPack.java
+++ src/main/java/flower_store/FlowerPack.java
@@ -1,9 +1,9 @@
 package flower_store;
 
+/**
+ * FlowerPack is a group of flowers.
+ */
 public class FlowerPack {
     private Flower flower;
     private int quantity;
-
-    //    private Flower flower;
-    //    private int quantity;
 }
