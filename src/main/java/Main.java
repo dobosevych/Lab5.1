192b41091112284be63d97edd23c42a80a5de8e7
dobosevych <dobosevych@gmail.com>
2022-10-13 11:10:57 +0300

    Fixed CI

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        boolean f = true || false;
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Hello!");
+        }
+    }
+}
