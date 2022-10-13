6da105ba923f85ce4e49164d66dbe4099e646e37
dobosevych <dobosevych@gmail.com>
2022-10-13 16:37:25 +0300

    Fixed CI

--- src/test/java/flower/store/FlowerTest.java
+++ src/test/java/flower/store/FlowerTest.java
@@ -3,10 +3,14 @@ package flower.store;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.Assertions;
 
 public class FlowerTest {
     private Flower flower;
+    private final int MAX_PRICE = 100;
+    private final Random RANDOM_GENERATOR = new Random();
 
     @BeforeEach
     public void init() {
@@ -15,7 +19,7 @@ public class FlowerTest {
 
     @Test
     public void testPrice() {
-        int price = 20;
+        int price = RANDOM_GENERATOR.nextInt(MAX_PRICE);
         flower.setPrice(price);
         Assertions.assertEquals(price, flower.getPrice());
     }
